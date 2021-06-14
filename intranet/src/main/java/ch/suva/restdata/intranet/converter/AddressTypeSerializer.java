14cf78fd7891cbb767531d37c969bb6acdcc6ff6
Schumacher Beat <sbs@suva.ch>
2021-06-14 08:30:15 +0200

    remove checked exceptions

--- intranet/src/main/java/ch/suva/restdata/intranet/converter/AddressTypeSerializer.java
+++ intranet/src/main/java/ch/suva/restdata/intranet/converter/AddressTypeSerializer.java
@@ -2,17 +2,16 @@ package ch.suva.restdata.intranet.converter;
 
 import ch.suva.restdata.intranet.entities.AddressType;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.google.common.base.Joiner;
-
-import java.io.IOException;
+import lombok.SneakyThrows;
 
 public class AddressTypeSerializer extends JsonSerializer<AddressType> {
     
+    @SneakyThrows
     @Override
-    public void serialize(AddressType value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+    public void serialize(AddressType value, JsonGenerator jgen, SerializerProvider provider) {
         jgen.writeObject(Joiner.on(" ").join(">", value, "<"));
     }
     
--- intranet/src/main/java/ch/suva/restdata/intranet/converter/KundenportalBenutzerSerializer.java
+++ intranet/src/main/java/ch/suva/restdata/intranet/converter/KundenportalBenutzerSerializer.java
@@ -2,17 +2,16 @@ package ch.suva.restdata.intranet.converter;
 
 import ch.suva.restdata.intranet.entities.KundenportalBenutzer;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.google.common.base.Joiner;
-
-import java.io.IOException;
+import lombok.SneakyThrows;
 
 public class KundenportalBenutzerSerializer extends JsonSerializer<KundenportalBenutzer> {
     
+    @SneakyThrows
     @Override
-    public void serialize(KundenportalBenutzer value, JsonGenerator jgen, SerializerProvider provider) throws IOException, JsonProcessingException {
+    public void serialize(KundenportalBenutzer value, JsonGenerator jgen, SerializerProvider provider) {
         jgen.writeStringField("name:", Joiner.on(" ").skipNulls()
             .join(value.getNachname(), value.getVorname()));
         jgen.writeObjectField("geschlecht", value.getGeschlecht());
